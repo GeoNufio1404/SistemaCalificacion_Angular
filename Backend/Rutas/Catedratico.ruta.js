f30444815552a3cbf9d15a3ce775a4d3dde6137d
kren007 <70988157+kren007@users.noreply.github.com>
2020-10-27 02:37:49 -0600

    Update Tabla Catedratico

--- Backend/Rutas/Catedratico.ruta.js
+++ Backend/Rutas/Catedratico.ruta.js
@@ -1,11 +1,11 @@
 var  express  =  require ( 'express' ) ;
-const  Usuarios  =  require ( '../Modelos/Usuarios.Modelo' ) ;
-var  UsuariosModelo  =  require ( '../Modelos/Usuarios.Modelo' ) ;
-var  UsuariosRuta  =  express . Enrutador ( ) ;
+const  Catedratico  =  require ( '../Modelos/Catedratico.Modelo' ) ;
+var  CatedraticoModelo  =  require ( '../Modelos/Catedratico.Modelo' ) ;
+var  CatedraticoRuta  =  express . Enrutador ( ) ;
 
 // Listar todos los elementos de una tabla
-UsuariosRuta . get ( '/ usuarios' ,  function ( req , res ) {
-   UsuariosModelo . selectAll ( function ( resultado ) {
+CatedraticoRuta . get ( '/ catedratico' ,  function ( req , res ) {
+    CatedraticoModelo . selectAll ( function ( resultado ) {
        if  (  resultado ! == undefined )  {
            res . json ( resultado ) ;
        }  más  {
@@ -15,9 +15,9 @@ UsuariosRuta . get ( '/ usuarios' ,  function ( req , res ) {
 } ) ;
 
 // Listar un elemento especifico
-UsuariosRuta . get ( '/ usuarios /: CarnetUsuario' ,  function ( req , res ) {
-    var  id  =  req . params . CarnetUsuario ;
-    UsuariosModelo . selectOne ( id , function ( resultado ) {
+CatedraticoRuta . get ( '/ catedratico /: NoCatedratico' ,  function ( req , res ) {
+    var  id  =  req . params . NoCatedratico ;
+    CatedraticoModelo . selectOne ( id , function ( resultado ) {
         if  ( resultado ! == undefined ) {
             res . json ( resultado ) ;
         } más {
@@ -27,9 +27,9 @@ UsuariosRuta . get ( '/ usuarios /: CarnetUsuario' ,  function ( req , res ) {
 } ) ;
 
 // Crear un nuevo elemento 
-UsuariosRuta . post ( '/ usuarios' ,  function ( req ,  res ) {
+CatedraticoRuta . post ( '/ catedratico' ,  function ( req ,  res ) {
     var  data  =  req . el cuerpo ;
-    UsuariosModelo . crear ( datos ,  función ( resultado ) {
+    CatedraticoModelo . crear ( datos ,  función ( resultado ) {
         si ( m resultado . affectedRows  >  0 ) {
             res . json ( resultado ) ;
         }  más  {
@@ -39,12 +39,12 @@ UsuariosRuta . post ( '/ usuarios' ,  function ( req ,  res ) {
 } ) ;
 
 // Reemplazar un nuevo elemento 
-UsuariosRuta . put ( '/ usuarios /: CarnetUsuario' ,  function ( req ,  res ) {
-    var  CarnetUsuario  =  req . params . CarnetUsuario ;
+CatedraticoRuta . put ( '/ catedratico /: NoCatedratico' ,  function ( req ,  res ) {
+    var  NoCatedratico  =  req . params . NoCatedratico ;
     var  data  =  req . el cuerpo ;
 
-    if ( CarnetUsuario  ==  data . CarnetUsuario ) {
-        UsuariosModelo . editar ( datos ,  función ( resultado ) {
+    if ( NoCatedratico  ==  data . NoCatedratico ) {
+        CatedraticoModelo . editar ( datos ,  función ( resultado ) {
             si ( m resultado . affectedRows  >  0 ) {
                 res . json ( resultado ) ;
             }  más  {
@@ -57,9 +57,9 @@ UsuariosRuta . put ( '/ usuarios /: CarnetUsuario' ,  function ( req ,  res ) {
 } ) ;
 
 // Eliminar un elemento
-UsuariosRuta . delete ( '/ usuarios /: CarnetUsuario' ,  function ( req ,  res ) {
-    var  CarnetUsuario  =  req . params . CarnetUsuario ;
-    UsuariosModelo . eliminar ( CarnetUsuario ,  función ( resultado ) {
+CatedraticoRuta . delete ( '/ catedratico /: NoCatedratico' ,  function ( req ,  res ) {
+    var  NoCatedratico  =  req . params . NoCatedratico ;
+    CatedraticoModelo . eliminar ( NoCatedratico ,  función ( resultado ) {
         si ( m resultado . affectedRows  >  0 ) {
             res . json ( resultado ) ;
         }  más  {
@@ -68,17 +68,4 @@ UsuariosRuta . delete ( '/ usuarios /: CarnetUsuario' ,  function ( req ,  res )
     } )
 } ) ;
 
-// Iniciar sesión (solo para esta tabla usuarios)
-UsuariosRuta . get ( '/ usuarios /: CarnetUsuario,: ​​PassUsuario' ,  function ( req ,  res ) {
-    var  CarnetUsuario  =  req . params . CarnetUsuario ;
-    var  PassUsuario  =  req . params . PassUsuario ;
-    UsuariosModelo . Validar ( CarnetUsuario ,  PassUsuario ,  function ( resultado ) {
-        if ( resultado ! == undefined ) {
-            res . json ( resultado ) ;
-        }  más  {
-            res . json ( { "mensaje" : "No hay para validar (iniciar sesión)" } )
-        }
-    } )
-} ) ;
-
-módulo . exportaciones  =  UsuariosRuta ;
+módulo . exportaciones  =  CatedraticoRuta ;
