0d8debd2cc06c137ba256e8ecf977bfa1f4ec974
201907136 <70353231+201907136@users.noreply.github.com>
2020-10-27 21:33:08 -0600

    Modelos Rebeca

---
+++ Backend/Modelos/Comentario.Modelo.js
@@ -0,0 +1,74 @@
+var db = require('../BaseDeDatos/db.config');
+var Comentario = {};
+
+//Seleccionar todos 
+Comentario.selectAll = function(callback){
+    if (db) {
+        var consulta = 'SELECT * FROM Comentario';
+        db.query(consulta, function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado);
+            }
+        })
+    }
+}
+
+//Seleccionar uno
+Comentario.selectOne = function(id,callback){
+    if (db) {
+        var consulta = 'SELECT * FROM Comentario where IDComentario = ?';
+        db.query(consulta, function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado[0]);
+            }
+        })
+    }
+}
+
+//Crear
+Comentario.create = function(data, callback) {
+    if (db) {
+        var consulta = "call sp_AgregarComentario(?,?,?,?)"
+        db.query (consulta,[data.IDComentario,data.Mensaje,data.Publicacion_IDPublicacion,data.Usuario_Carnet],function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado);
+            }
+        })
+    }
+}
+
+//Editar
+Comentario.edit = function(data, callback) {
+    if (db) {
+        var consulta = "call sp_EditarComentario(?,?,?,?)"
+        db.query (consulta,[data.IDComentario,data.Mensaje,data.Publicacion_IDPublicacion,data.Usuario_Carnet],function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado);
+            }
+        })
+    }
+}
+
+//Eliminar
+Comentario.delete = function(data, callback) {
+    if (db) {
+        var consulta = "call sp_EliminarComentario(?)"
+        db.query (consulta,[data.IDComentario],function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado);
+            }
+        })
+    }
+}
+
+module.exports = Comentario;
---
+++ Backend/Modelos/Publicacion.Modelo.js
@@ -0,0 +1,74 @@
+var db = require('../BaseDeDatos/db.config');
+var Publicacion = {};
+
+//Seleccionar todos 
+Publicacion.selectAll = function(callback){
+    if (db) {
+        var consulta = 'SELECT * FROM Publicacion';
+        db.query(consulta, function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado);
+            }
+        })
+    }
+}
+
+//Seleccionar uno
+Publicacion.selectOne = function(id,callback){
+    if (db) {
+        var consulta = 'SELECT * FROM Publicacion where IDPublicacion = ?';
+        db.query(consulta, function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado[0]);
+            }
+        })
+    }
+}
+
+//Crear
+Publicacion.create = function(data, callback) {
+    if (db) {
+        var consulta = "call sp_AgregarPublicacion(?,?,?,?,?,?,?,?)"
+        db.query (consulta,[data.IDPublicacion,data.mensaje,data.Usuario_Carnet,data.Fecha,data.Curso_Catedratico_idCatedratico,data.Curso_CodigoCurso,data.Catedratico_NoCatedratico,data.Tipo],function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado);
+            }
+        })
+    }
+}
+
+//Editar
+Publicacion.edit = function(data, callback) {
+    if (db) {
+        var consulta = "call sp_EditarPublicacion(?,?,?,?,?,?,?,?)"
+        db.query (consulta,[data.IDPublicacion,data.mensaje,data.Usuario_Carnet,data.Fecha,data.Curso_Catedratico_idCatedratico,data.Curso_CodigoCurso,data.Catedratico_NoCatedratico,data.Tipo],function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado);
+            }
+        })
+    }
+}
+
+//Eliminar
+Publicacion.delete = function(data, callback) {
+    if (db) {
+        var consulta = "call sp_EliminarPublicacion(?)"
+        db.query (consulta,[data.IDPublicacion],function(error,resultado){
+            if (error) {
+                throw error;
+            } else {
+                callback(resultado);
+            }
+        })
+    }
+}
+
+module.exports = Publicacion;
