cdd29b700e6bbb371341642469463fe7e89120a8
Voupi <voupi.sfh@gmail.com>
2020-11-01 19:08:29 -0600

    Inicio de Sesión

--- Frontend/client/src/app/app-routing.module.ts
+++ Frontend/client/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PerfilComponent} from './componets/perfil/perfil.component'
+import {InicioSesionComponent} from './componets/inicio-sesion/inicio-sesion.component'
+import {RegistroUsuarioComponent} from './componets/registro-usuario/registro-usuario.component'
+const routes: Routes = [
+  {
+    path: '',//Esto redirecciona cuando ha iniciado la página
+    redirectTo: '/inicio-sesion'//Redirecciona al perfil
+    ,pathMatch: 'full'
+  },
+  {
+    path: 'inicio-sesion',//
+    component: InicioSesionComponent
+  },
+  {
+    path: 'registro-usuario',//
+    component: RegistroUsuarioComponent
+  }
+  ,
+  {
+    path: 'perfil',//
+    component: RegistroUsuarioComponent
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/client/src/app/app.module.ts
+++ Frontend/client/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './componets/navigation/navigation.component';
+import { PerfilComponent } from './componets/perfil/perfil.component';
+import { InicioSesionComponent } from './componets/inicio-sesion/inicio-sesion.component';
+import { RegistroUsuarioComponent } from './componets/registro-usuario/registro-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    PerfilComponent,
+    InicioSesionComponent,
+    RegistroUsuarioComponent
   ],
   imports: [
     BrowserModule,
