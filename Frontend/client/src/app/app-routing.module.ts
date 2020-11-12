1998500d6722dad61484fd9aa8eed32ce91b34a1
BelenContrerasOrozco <70045067+BelenContrerasOrozco@users.noreply.github.com>
2020-11-11 18:08:29 -0600

    Comentarios

--- Frontend/client/src/app/app-routing.module.ts
+++ Frontend/client/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import {PerfilComponent} from './componets/perfil/perfil.component'
 import {InicioSesionComponent} from './componets/inicio-sesion/inicio-sesion.component'
 import {RegistroUsuarioComponent} from './componets/registro-usuario/registro-usuario.component'
 import { OlvContrComponent } from "./componets/olv-contr/olv-contr.component";
+import {PantallaInicialComponent} from  "./components/pantalla-inicial/pantalla-inicial.component";
+import {ComentariosComponent} from "./components/comentarios/comantarios.component";
 const routes: Routes = [
   {
     path: '',//Esto redirecciona cuando ha iniciado la página
